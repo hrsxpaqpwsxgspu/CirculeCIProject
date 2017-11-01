0a77c778fea410467211e31e40458c7fd3662dbd
dudi938 <dudi938@gmail.com>
2017-11-01 12:02:38 +0200

    first commit

---
+++ circuleCITest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circuleCITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First circule CI");
+            Console.ReadKey();
+        }
+    }
+}
