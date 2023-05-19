e77665e15a931050f0ecb3eb197fdd2385ac10cc
hrsxpaqpwsxgspu <134038200+hrsxpaqpwsxgspu@users.noreply.github.com>
2023-05-19 07:18:45 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -28,7 +28,7 @@ import subprocess
 
 #@markdown  Visit http://remotedesktop.google.com/headless and copy the command after Authentication
 
-CRP = "DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"4/0AbUR2VNWm9F_wmuDpnxHExCiMwSOKj0Mb22SjJoZqfmQNPt-qRzPMczGACvACALavL_HBQ\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=$(hostname)"
+CRP = "DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"4/0AbUR2VMu2EseO_HahAiGrDZGzFDdtW9oMWZ6dmZYtVpzrysK8gk88QxxOmrjzkjRr2o75g\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=$(hostname)"
 #@markdown Enter a Pin (more or equal to 6 digits)
 Pin = 123456 #@param {type: "integer"}
 
