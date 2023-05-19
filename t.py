e422b7e05790bbe136aee0ea5d96e2622e645b92
hrsxpaqpwsxgspu <134038200+hrsxpaqpwsxgspu@users.noreply.github.com>
2023-05-19 09:06:15 +0000

    Update t.py

--- t.py
+++ t.py
@@ -2,10 +2,10 @@ import time
 alive = True #@param {type:"boolean"}
 
 temps = 0
-sec = 3600
-while(temps < 43200):
+sec = 36
+while(temps < 4320000):
   if(temps == sec):
-    sec += 3600
+    sec += 36
     x = (temps/60)/60
     print("It's been ",x,"  hour...")
   time.sleep(1)
