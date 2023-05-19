f2610806469cd1627104ae913b80b322191e5724
hrsxpaqpwsxgspu <134038200+hrsxpaqpwsxgspu@users.noreply.github.com>
2023-05-19 07:43:06 +0000

    Update 1.py
    update

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 import os
-from colab_ssh import launch_ssh
 
 username = "user" #@param {type:"string"}
 password = "root" #@param {type:"string"}
@@ -104,49 +103,3 @@ try:
         print("Enter a pin more or equal to 6 digits")
     else:
         CRD(username)
-except NameError as e:
-    print("'username' variable not found, Create a user first")
-    
-    
-    
-    #@title **SSH**
-
- 
-
-#@markdown Choose a method (Agro Recommended)
-ssh_method = "Ngrok" #@param ["Agro", "Ngrok"]
-
-
-#@markdown Copy authtoken from https://dashboard.ngrok.com/auth (only for ngrok)
-ngrokRegion = "us" #@param ["us", "eu", "ap", "au", "sa", "jp", "in"]
-
-def runAgro():
-    from colab_ssh import launch_ssh_cloudflared
-    launch_ssh_cloudflared(password=password)
-
-def runNgrok():
-    from colab_ssh import launch_ssh
-    from IPython.display import clear_output
-
-    import getpass
-    ngrokToken = getpass.getpass("Enter the ngrokToken: ")
-
-    launch_ssh(ngrokToken, password, region=ngrokRegion)
-    clear_output()
-
-    print("ssh", user, end='@')
-    
-
-try:
-    user = username
-    password = password
-except NameError:
-    print("No user found, using username and password as 'root'")
-    user='root'
-    password='root'
-
-
-if ssh_method == "Agro":
-    runAgro()
-if ssh_method == "Ngrok":
-    runNgrok()
